232feea127a8fbeb96f381f6956e8c6dc072c5dc
Mustafa Aamir <mustafa.290101@gmail.com>
2024-12-25 07:46:26 +0400

    added README.md

--- README.md
+++ README.md
@@ -1,12 +1,85 @@
 # fifth
-The sequel to forth nobody wished for
+The sequel to Forth nobody wished for
 
 # Preface
 
-Aside from the obvious forth inspiration, i'll make up design decisions along the way
+Fifth™ might not be the most practical language ever created, but it's certainly one of the most stack-based ones created in the last five minutes. Use it wisely, or at least use it in a way that produces interesting error messages. Divide by zero. Have fun.
 
+- Instructions are stored in a list of opcodes.
+- Some instructions are product types of opcodes.
+- Hack away in the `bin/` directory.
 
+# Installation
+```
+$ Copy and paste every file in in a new directory.
+$ Especially the ones I forgot to add to my `.gitignore`.
+$ bash -c "sh <(curl -fsSL https://raw.githubusercontent.com/ocaml/opam/master/shell/install.sh)"
+$ opam switch create .
+$ opam install ocaml dune
+$ dune build @fmt
+$ open bin/main.ml in a notepad++
+$ dune exec -w fifth
+$ Write your own programs in bin/main.ml and inspect the output without having to recompile
+```
+
+# Overview
+
+### Opcodes:
+```
+  Push of int -> Push 1
+  Pop
+  Add
+  Sub
+  Mul
+  Div
+  Equal
+  Dump
+  Dup
+  Halt -> ends the program
+  If of opcode list * opcode list -> If([then_block], [else_block])
+  DeFun of string * opcode list -> Defun (<name>, [function_block])
+  CallFun of string -> CallFun <name>
+```
+### Conditionals
+
+#### Assertion:
+```ocaml
+[Push 42; Push 42; Equal]  (* stack: 1 *)
+[Push 42; Push 41; Equal]  (* stack: 0 *)
+```
+#### Conditional execution:
+```ocaml
+let program = [Push 0; If([Push 100; Dump], [Push 200; Dump]); Halt]
+```
+The value at the top of the stack is evaluated as the condition for the subsequent `if` statement.
+If the value is 1 (true), execute the `then_block`, which is prints 100. Otherwise, print 200.
+In this example it prints 200, but if that upsets you, we can change the condition to 1.
+
+```ocaml
+let program = [Push 1; If([Push 100; Dump], [Push 200; Dump]); Halt] (* prints 100 *)
+```
+
+### Functions
+
+Let's define a function to square a number.
+```ocaml
+[
+  (* Define a function that squares a number *)
+  DeFun ("square",
+    [
+      Dup;    (* Duplicate the input *)
+      Mul     (* Multiply it by itself *)
+    ]
+  );
+
+  Push 5;
+  CallFun "square";  (* stack: 25 *)
+  Dump
+]
+=> 25 (* 5 * 5 *)
+```
 
 # TODO
 
-1. Use cmdliner, Filename, Stdio, Arg for cli
+1. Explore arg for CLI
+2. Menhir or Regex for parsing. I hate writing parsers.
--- lib/fifth.ml
+++ lib/fifth.ml
@@ -1,9 +1,17 @@
 type opcode =
-  | Push of int | Pop
-  | Add | Sub | Mul
-  | Div | Equal | Dump
-  | Halt | If of opcode list * opcode list
-  | Dup | DeFun of string * opcode list | CallFun of string
+  | Push of int
+  | Pop
+  | Add
+  | Sub
+  | Mul
+  | Div
+  | Equal
+  | Dump
+  | Halt
+  | If of opcode list * opcode list
+  | Dup
+  | DeFun of string * opcode list
+  | CallFun of string
 
 type program = opcode list
 
@@ -38,33 +46,60 @@ let rec simulate program acc =
   | [] -> ()
   | x :: xs -> (
       match x with
-      | Push n -> Stack.push n stack; simulate xs (acc + 1)
-      | Pop -> ( match Stack.pop_opt stack with | Some _ -> simulate xs (acc + 1) | None -> "Pop operation being performed on empty stack" |> failwith)
-      | Dup -> (match Stack.top_opt stack with | Some v -> Stack.push v stack | None -> "Empty stack. Can't do Dup, Dupbass." |> failwith); simulate xs (acc + 1)
-      | Add -> arith_bin_op ( + ) stack; simulate xs (acc + 1)
-      | Mul -> arith_bin_op ( * ) stack; simulate xs (acc + 1)
-      | Sub -> arith_bin_op ( - ) stack; simulate xs (acc + 1)
-      | Equal -> logical_bin_op ( = ) stack; simulate xs (acc + 1)
+      | Push n ->
+          Stack.push n stack;
+          simulate xs (acc + 1)
+      | Pop -> (
+          match Stack.pop_opt stack with
+          | Some _ -> simulate xs (acc + 1)
+          | None -> "Pop operation being performed on empty stack" |> failwith)
+      | Dup ->
+          (match Stack.top_opt stack with
+          | Some v -> Stack.push v stack
+          | None -> "Empty stack. Can't do Dup, Dupbass." |> failwith);
+          simulate xs (acc + 1)
+      | Add ->
+          arith_bin_op ( + ) stack;
+          simulate xs (acc + 1)
+      | Mul ->
+          arith_bin_op ( * ) stack;
+          simulate xs (acc + 1)
+      | Sub ->
+          arith_bin_op ( - ) stack;
+          simulate xs (acc + 1)
+      | Equal ->
+          logical_bin_op ( = ) stack;
+          simulate xs (acc + 1)
       | Dump -> (
           match Stack.top_opt stack with
-          | Some v -> Printf.printf "%d\n" v; simulate xs (acc + 1)
-          | None -> "Dump operation can't be performed on an empty stack" |> failwith)
+          | Some v ->
+              Printf.printf "%d\n" v;
+              simulate xs (acc + 1)
+          | None ->
+              "Dump operation can't be performed on an empty stack" |> failwith)
       | Div ->
           (match Stack.pop_opt stack with
           | Some a when a <> 0 -> (
               match Stack.pop_opt stack with
               | Some b -> Stack.push (b / a) stack
               | None -> "Second operand isn't present in the stack" |> failwith)
-          | None -> "Binary operation being performed on empty stack" |> failwith
+          | None ->
+              "Binary operation being performed on empty stack" |> failwith
           | _ -> "Numerator cannot be zero" |> failwith);
           simulate xs (acc + 1)
       | If (then_branch, else_branch) -> (
           match Stack.pop_opt stack with
-          | Some v -> if v <> 0 then simulate then_branch acc else simulate else_branch acc
+          | Some v ->
+              if v <> 0 then simulate then_branch acc
+              else simulate else_branch acc
           | None -> raise_so "If condition on line %d with empty stack" acc)
-      | DeFun (name, body) -> Hashtbl.add function_table name body; simulate xs (acc + 1)
+      | DeFun (name, body) ->
+          Hashtbl.add function_table name body;
+          simulate xs (acc + 1)
       | CallFun name -> (
           match Hashtbl.find_opt function_table name with
-          | Some body -> simulate body acc; simulate xs (acc + 1)
+          | Some body ->
+              simulate body acc;
+              simulate xs (acc + 1)
           | None -> "Undefined function " ^ name |> failwith)
       | Halt -> ())
